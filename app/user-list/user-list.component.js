58b7907b20efc97822b8c86c16a413fa07b2564d
Mikhail Sigarev <mikhail_sigarev@epam.com>
2017-02-06 09:52:53 +0300

    added http request to get a user list

--- app/user-list/user-list.component.js
+++ app/user-list/user-list.component.js
@@ -2,35 +2,12 @@ angular.
   module('userApp').
   component('userList', {
     templateUrl: 'user-list/user-list.template.html',
-    controller: function UserListController() {
-      this.users = [
-        {
-          isid: 'm',
-          givenname: 'm',
-          familyname: "m",
-          password: "m",
-          username: "m",
-          email: "m@m.m",
-          age: 25
-        }, {
-          isid: 'hodor3',
-          givenname: 'hodor',
-          familyname: "hodor",
-          password: "hodor",
-          username: "hodor",
-          email: "hodor@gmail.com",
-          age: 30
-        }, {
-          isid: 'pepe',
-          givenname: 'pepe',
-          familyname: "pepe",
-          password: "pepe",
-          username: "pepe",
-          email: "pepe@mail.ru",
-          age: 35
-        }
-     ];
+    controller: function UserListController($http) {
+      const self = this;
+      self.orderProp = 'age';
 
-     this.orderProp = 'age';
+      $http.get('users/users.json').then(function(resp) {
+        self.users = resp.data;
+      });
     }
   });
