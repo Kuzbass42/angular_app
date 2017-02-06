580876dbbaa6219ef5a14634e4c9b8e737593224
Mikhail Sigarev <mikhail_sigarev@epam.com>
2017-02-06 09:45:15 +0300

    added order

--- app/user-list/user-list.component.js
+++ app/user-list/user-list.component.js
@@ -10,22 +10,27 @@ angular.
           familyname: "m",
           password: "m",
           username: "m",
-          email: "m@m.m"
+          email: "m@m.m",
+          age: 25
         }, {
           isid: 'hodor3',
           givenname: 'hodor',
           familyname: "hodor",
           password: "hodor",
           username: "hodor",
-          email: "hodor@gmail.com"
+          email: "hodor@gmail.com",
+          age: 30
         }, {
           isid: 'pepe',
           givenname: 'pepe',
           familyname: "pepe",
           password: "pepe",
           username: "pepe",
-          email: "pepe@mail.ru"
+          email: "pepe@mail.ru",
+          age: 35
         }
      ];
+
+     this.orderProp = 'age';
     }
   });
